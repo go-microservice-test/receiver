d65217d53ea699950b5999d3f24f6ec16706ba8b
Jack0O7 <dastan.nomad@gmail.com>
2024-06-12 17:49:01 +0500

    added basic GET all, GET by id, POST handlers

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Item - some abstract data holding type.
+type Item string
+
+// JSONItem - map item processed into JSON parseable object.
+type JSONItem struct {
+	ID   int  `json:"id"`
+	Item Item `json:"data"`
+}
+
+// JSONItemInput - item input json format
+type JSONItemInput struct {
+	Data string `json:"data"`
+}
+
+var (
+	items     = make(map[int]Item) // mapping from ID to Item
+	idCounter = 1                  // counter for the ids
+	mu        sync.Mutex           // DB mutex
+)
+
+func main() {
+	// get an engine instance
+	r := gin.Default()
+
+	// connect routes
+	r.GET("/items", getItems)
+	r.GET("/items/:id", getItem)
+	r.POST("/items", createItem)
+
+	// run the server
+	r.Run(":3000")
+}
+
+func getItems(c *gin.Context) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	// convert map into JSON parseable format
+	var resItemList []JSONItem
+	for id, item := range items {
+		resItemList = append(resItemList, JSONItem{ID: id, Item: item})
+	}
+	// return all items
+	c.JSON(http.StatusOK, resItemList)
+}
+
+func getItem(c *gin.Context) {
+	// retrieving URL id param
+	id, err := strconv.Atoi(c.Param("id"))
+	// invalid id
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID must be a number"})
+		return
+	}
+	mu.Lock()
+	defer mu.Unlock()
+
+	// retrieve an item with id
+	item, ok := items[id]
+	// item does not exist
+	if !ok {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Item not found"})
+		return
+	}
+	// send the requested item
+	c.JSON(http.StatusOK, JSONItem{ID: id, Item: item})
+}
+
+func createItem(c *gin.Context) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	var item JSONItemInput
+	// incorrect input format handling
+	if err := c.ShouldBindJSON(&item); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// add new item with the correct id
+	var processedItem = Item(item.Data)
+	items[idCounter] = processedItem
+	// return created item
+	c.JSON(http.StatusOK, JSONItem{ID: idCounter, Item: processedItem})
+	// update the counter
+	idCounter++
+}
